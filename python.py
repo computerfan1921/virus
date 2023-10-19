f5a902344071a0fcbae9641ce6ab405e9ba210ea
compu terfan 1921 <148014224+computerfan1921@users.noreply.github.com>
2023-10-19 12:05:43 +1100

    Create python.py
    i copy pasted this code from stack overflow

---
+++ python.py
@@ -0,0 +1,10 @@
+import sys
+import subprocess
+
+procs = []
+for i in range(86):
+    proc = subprocess.Popen([sys.executable, 'task.py', '{}in.csv'.format(i), '{}out.csv'.format(i)])
+    procs.append(proc)
+
+for proc in procs:
+    proc.wait()
