6e82c4b9a3305dbc208615f30694738d546931ba
compu terfan 1921 <148014224+computerfan1921@users.noreply.github.com>
2023-10-20 02:08:22 +1100

    also cancel ed due to copyright

--- python.py
+++
@@ -1,8 +0,0 @@
-import sys
-import subprocess
-procs = []
-for i in range(86):
-    proc = subprocess.Popen([sys.executable, 'python.py', '{}in.csv'.format(i), '{}out.csv'.format(i)])
-    procs.append(proc)
-for proc in procs:
-    proc.wait()
