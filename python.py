0ece3c8850d5147d5c40c26fa0a1f49a43b63a3c
compu terfan 1921 <148014224+computerfan1921@users.noreply.github.com>
2023-10-19 12:26:06 +1100

    Make code more efficient
    aakmcvmxc

--- python.py
+++ python.py
@@ -1,10 +1,8 @@
 import sys
 import subprocess
-
 procs = []
 for i in range(86):
     proc = subprocess.Popen([sys.executable, 'python.py', '{}in.csv'.format(i), '{}out.csv'.format(i)])
     procs.append(proc)
-
 for proc in procs:
     proc.wait()
